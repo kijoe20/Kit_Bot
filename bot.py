ba0eb88d9eca22bbfee8e764402cc78c75d920d3
kijoe20 <82643980+kijoe20@users.noreply.github.com>
2022-02-22 18:31:03 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+from msilib.schema import File
 import discord
 from discord.ext import commands
 import json
+import random
 
 with open('setting.json','r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -10,6 +12,7 @@ bot = commands.Bot(command_prefix='[')
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
+
 
 @bot.event
 async def on_member_join(member):
@@ -25,5 +28,16 @@ async def on_member_join(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+@bot.command()
+async def 圖片(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file= pic)
+
+@bot.command()
+async def web(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
 
 bot.run(jdata['TOKEN'])
